daa9f9ac0341cd142f88d6ee3b134dd560d8d239
Udalov Max <re.udalov@gmail.com>
2019-09-12 18:45:22 +0300

    add gitconfig

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 ln -sf $(pwd)/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/.vimrc.plugins $HOME/.vimrc.plugins
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
 
+cat >> $HOME/.gitconfig <<EOF
+[include]
+    path = $(pwd)/.gitconfig
+EOF
