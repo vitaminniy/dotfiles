1853c0bc7edea9423de5beff953270718b48a159
Udalov Max <re.udalov@gmail.com>
2019-09-12 10:59:41 +0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+ln -sf $(pwd)/.vimrc $HOME/.vimrc
+ln -sf $(pwd)/.vimrc.plugins $HOME/.vimrc.plugins
+ln -sf $(pwd)/.zshrc $HOME/.zshrc
+
