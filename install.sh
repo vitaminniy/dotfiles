587d246f65a5152d0fccd01cbc7104227c1f057e
Udalov Max <re.udalov@gmail.com>
2019-11-12 15:29:20 +0300

    alacritty.yml,install.sh: update alacritty configuration

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.v
 ln -sf $(pwd)/zshrc $HOME/.zshrc
 ln -sf $(pwd)/vimrc $HOME/.vimrc
 ln -sf $(pwd)/vimrc.plugins $HOME/.vimrc.plugins
-ln -sf $(pwd)/alacritty.yml $HOME/.alacritty.yml
+
+mkdir -p $HOME/.config/alacritty
+ln -sf $(pwd)/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 cat >> $HOME/.gitconfig <<EOF
 [include]
