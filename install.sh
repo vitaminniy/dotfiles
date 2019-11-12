6a1ffa3e319f78b78c56c4fdb68ba55aecc4d6fc
Udalov Max <re.udalov@gmail.com>
2019-11-12 11:32:36 +0300

    renamed .rc files

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-ln -sf $(pwd)/.vimrc $HOME/.vimrc
-ln -sf $(pwd)/.vimrc.plugins $HOME/.vimrc.plugins
-ln -sf $(pwd)/.zshrc $HOME/.zshrc
+ln -sf $(pwd)/zshrc $HOME/.zshrc
+ln -sf $(pwd)/vimrc $HOME/.vimrc
+ln -sf $(pwd)/vimrc.plugins $HOME/.vimrc.plugins
 ln -sf $(pwd)/alacritty.yml $HOME/.alacritty.yml
 
 cat >> $HOME/.gitconfig <<EOF
