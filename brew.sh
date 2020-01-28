da6c161a10ac2d19df6941233978f69768f69519
Udalov Max <re.udalov@gmail.com>
2020-01-28 11:11:32 +0300

    brewbins: rename to brew.sh

---
+++ brew.sh
@@ -0,0 +1,12 @@
+#! /bin/env sh
+
+brew install \
+         htop \
+         tmux \
+         jq \
+         jrnl
+
+# brew head-only formulas
+brew install --HEAD \
+    universal-ctags/universal-ctags/universal-ctags
+
