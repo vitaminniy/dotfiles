bc0300b07c2f5c4809af9cdf8682fbe3088e334b
Udalov Max <re.udalov@gmail.com>
2020-07-13 10:28:25 +0300

    bin: fix vim configuration script

--- bin/configure_vim.bash
+++ bin/configure_vim.bash
@@ -2,7 +2,5 @@
 
 ./configure --with-features=huge \
 			--enable-multibyte --enable-cscope \
-			--enable-python3interp=yes \
-			--with-python3-config-dir=$(python3-config --configdir) \
 			--enable-luainterp=yes-enable-luainterp=yes
 
