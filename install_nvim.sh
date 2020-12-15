b723959ba3c8e75a161ab13e57beb5677259e5d1
Udalov Max <re.udalov@gmail.com>
2020-12-16 00:37:14 +0300

    install_nvim: add coc-settings

--- install_nvim.sh
+++ install_nvim.sh
@@ -4,6 +4,7 @@
 mkdir -p $HOME/.config/nvim
 
 ln -sf $(pwd)/init.vim $HOME/.config/nvim/init.vim
+ln -sf $(pwd)/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
 # install plug
 sh -c 'curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
