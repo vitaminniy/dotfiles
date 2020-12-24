8e253f1a8d1d2172ca59f58ae1c17760082041d8
Udalov Max <re.udalov@gmail.com>
2020-12-25 01:11:20 +0300

    Add rust disrectory
    Add rust disrectory with rust-lang specific tools.

---
+++ rust/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+main() {
+    # install rustup
+    if ! command -v rustup; then
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    fi
+
+    if ! -d $HOME/.cargo/bin; then
+        mkdir -p $HOME/.cargo/bin
+    fi
+
+    # add components
+    rustup component add rust-src
+
+    # install tools
+    cargo install rusty-tags # ctags generation
+}
+
+main || exit 1
