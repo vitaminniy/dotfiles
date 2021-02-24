c5bd21dc514fbc8cd65bf733f06a5aa449a23586
Udalov Max <re.udalov@gmail.com>
2021-02-24 10:52:15 +0300

    brew: housekeeping

--- brew.sh
+++ brew.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-brew	install			\
-		ccache			\
-		cmake			\
-		conan			\
-		coreutils		\
-		fzf				\
-		grpc			\
-		htop			\
-		jq				\
-		llvm			\
-		ncdu			\
-		node			\
-		pinentry-mac	\
-		protobuf		\
-		pyenv			\
-		stow			\
-		tmux			\
-		yarn
+brew    install         \
+        ccache          \
+        cmake           \
+        conan           \
+        coreutils       \
+        fzf             \
+        htop            \
+        jq              \
+        llvm            \
+        ncdu            \
+        node            \
+        pinentry-mac    \
+        pyenv           \
+        stow            \
+        tmux            \
+        yarn
 
