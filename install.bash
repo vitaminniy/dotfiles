793cbfa7e9a6c3a4ade868f365fe01de1fb06f9f
Udalov Max <re.udalov@gmail.com>
2021-09-24 13:17:04 +0300

    install: fix nvim installation script

--- install.bash
+++ install.bash
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 install_nvim() {
-    NVIM_PLUG_FILE=$HOME/.config/nvim/autoload/plug.vim
-    if [ ! -f $NVIM_PLUG_FILE ]; then
+    NVIM_PLUG_PATH=$HOME/.config/nvim/autoload
+    NVIM_PLUG_FILE=$NVIM_PLUG_PATH/plug.vim
+
+    if [[ ! -d "$NVIM_PLUG_PATH" ]]; then
+        mkdir -p $HOME/.config/nvim/autoload
+    fi
+
+    if [[ ! -f "$NVIM_PLUG_FILE" ]]; then
         echo "installing neovim plug"
         sh -c "curl -fLo $NVIM_PLUG_FILE --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
