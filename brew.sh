b9eeecc749f85ae1d32a38ce1a076d05d4941559
Udalov Max <re.udalov@gmail.com>
2021-10-25 16:00:00 +0300

    brew: add prototool

--- brew.sh
+++ brew.sh
@@ -16,6 +16,7 @@ brew    install         \
         node            \
         pinentry-mac    \
         postgres        \
+        prototool       \
         pyenv           \
         stow            \
         tmux            \
