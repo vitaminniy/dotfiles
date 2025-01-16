f5f87ac3cc8a932d43492e1e0364b4c92ee6c222
Max Udalov <re.udalov@gmail.com>
2025-01-16 11:03:21 +0400

    brew: drop unused deps

--- brew.sh
+++ brew.sh
@@ -3,24 +3,18 @@
 brew    install          \
         autoconf         \
         automake         \
-        ccache           \
         cmake            \
         cmake-docs       \
-        conan            \
         coreutils        \
         fzf              \
         gnupg            \
         htop             \
         jq               \
-        llvm             \
-        ncdu             \
-        node             \
         pinentry-mac     \
         pyenv            \
         rbenv ruby-build \
         ripgrep          \
         stow             \
         tmux             \
-        wget             \
-        yarn
+        wget             
 
