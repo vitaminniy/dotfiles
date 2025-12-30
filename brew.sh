296ccdbdbbead427fa3fde6a1deb91f855e86b73
Udalov Max <re.udalov@gmail.com>
2025-12-30 21:42:56 +0400

    brew: cleanup

--- brew.sh
+++ brew.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
 brew    install             \
-        autoconf            \
-        automake            \
-        bat                 \
-        cmake               \
-        cmake-docs          \
         coreutils           \
         fzf                 \
-        gnupg               \
-        htop                \
         jq                  \
-        lua-language-server \
-        perl                \
-        pinentry-mac        \
-        pyenv               \
-        rbenv               \
         ripgrep             \
-        ripgrep             \
-        ruby-build          \
         stow                \
-        the_silver_searcher \
         tmux                \
         universal-ctags     \
         wget
